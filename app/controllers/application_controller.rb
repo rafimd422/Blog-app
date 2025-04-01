95d57d58a5202133ffdf377ab8870c16a4214e56
rafimd422 <rafimd2222@gmail.com>
2025-04-01 12:34:11 +0600

    created a post request with dynamic id

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,2 @@
 class ApplicationController < ActionController::Base
-def index
-  @blog_posts = BlogPost.all
-end
 end
--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,2 +1,11 @@
 class BlogPostsController < ApplicationController
+    def index
+      @blog_posts = BlogPost.all
+    end
+
+    def show
+      @blog_post = BlogPost.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        redirect_to root_path, alert: "Blog post not found."
+    end
 end
