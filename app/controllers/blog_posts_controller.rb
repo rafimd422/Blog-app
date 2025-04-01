d9136db611eba3d983ca409f070e123d4d5e4075
rafimd422 <rafimd2222@gmail.com>
2025-04-02 01:53:26 +0600

    added a new blog post action

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,11 +1,18 @@
 class BlogPostsController < ApplicationController
+    # to show all blog post
     def index
       @blog_posts = BlogPost.all
     end
 
+    # to show specific blog post
     def show
       @blog_post = BlogPost.find(params[:id])
       rescue ActiveRecord::RecordNotFound
         redirect_to root_path, alert: "Blog post not found."
+    end
+
+    # to create new blogpost
+    def new
+      @blog_post = BlogPost.new
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-get "blog_post/:id", to: "blog_posts#show"
-
-root "blog_posts#index"
+    get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
+    get "blog_post/:id", to: "blog_posts#show"
+    post "blog_posts", to: "blog_posts#create", as: :blog_posts
+    root "blog_posts#index"
 end
