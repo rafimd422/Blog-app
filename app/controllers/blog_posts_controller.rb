ca665a15096119afb8fd08a21c8f6cbb7d3aebe1
rafimd422 <rafimd2222@gmail.com>
2025-04-02 16:52:43 +0600

    added new post request and create function

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,7 +1,7 @@
 class BlogPostsController < ApplicationController
     # to show all blog post
     def index
-      @blog_posts = BlogPost.all
+      @blog_posts = BlogPost.all # Fetch all blog posts from the database
     end
 
     # to show specific blog post
@@ -13,6 +13,19 @@ class BlogPostsController < ApplicationController
 
     # to create new blogpost
     def new
-      @blog_post = BlogPost.new
+      @blog_post = BlogPost.new # Initialize a new BlogPost object
+    end
+
+    def create
+      @blog_post = BlogPost.new(blog_post_params)
+      if @blog_post.save
+        redirect_to @blog_post # Redirect to the show page of the newly created blog post
+      else
+        render :new # Render the new template again if there are errors
+      end
+    end
+
+    def blog_post_params
+      params.require(:blog_post).permit(:title, :body)
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
     get "/blog_posts/new", to: "blog_posts#new", as: :new_blog_post
-    get "blog_post/:id", to: "blog_posts#show"
-    post "blog_posts", to: "blog_posts#create", as: :blog_posts
+    get "blog_post/:id", to: "blog_posts#show", as: :blog_post
+    post "/blog_posts", to: "blog_posts#create", as: :blog_posts
     root "blog_posts#index"
 end
